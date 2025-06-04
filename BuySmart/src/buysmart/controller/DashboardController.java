6d3130a013ce285ccafdd0413668530a2defd14e
Aaryaman01 <aaryamanshakya11@gmail.com>
2025-06-04 09:36:28 +0545

    added ActionListener code

--- BuySmart/src/buysmart/controller/DashboardController.java
+++ BuySmart/src/buysmart/controller/DashboardController.java
@@ -5,6 +5,10 @@
 package buysmart.controller;
 
 import buysmart.view.Dashboard;
+import buysmart.view.LoginView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 /**
  *
@@ -15,6 +19,10 @@ public class DashboardController {
 
     public DashboardController(Dashboard dashboard){
         this.dashboard=dashboard;
+        
+        Logout logout = new Logout();
+        this.dashboard.logout(logout);
+                
     }
 
     public void open(){
@@ -23,8 +31,20 @@ public class DashboardController {
 
     public void close(){
         dashboard.dispose();
-    }
+    }  
+    
+    // Logoutbutton Function
+    class Logout implements ActionListener{
 
-    
-    
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dashboard.dispose();
+            LoginView loginview = new LoginView();
+            LoginController loginController = new LoginController(loginview);
+            loginController.open();
+            
+        }
+        
+        
+    }
 }
