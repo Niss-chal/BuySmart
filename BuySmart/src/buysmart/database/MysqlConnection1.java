d4157023119d2e23d6d5d632539bf1cf3844ea74
Avay Lama <avaytamang27@gmail.com>
2025-06-04 08:37:51 +0545

    done connection with MySQL database named 'buysmart' and Mysql JDBC drive using MySQL connector jars package.

---
+++ BuySmart/src/buysmart/database/MysqlConnection1.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buysmart.database;
+
+/**
+ *
+ * @author fahmi
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlConnection1 {
+    private static final String URL = "jdbc:mysql://localhost:3306/buysmart?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Captainrifal33@";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver bhetena ta. Aba connector JAR bhanne package hala project maa.", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = MysqlConnection1.getConnection();
+            System.out.println("Connection bhayo hai saathi!!");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Connection Bhayena ta babu!!");
+        }
+    }
+}
+
+
+
+
