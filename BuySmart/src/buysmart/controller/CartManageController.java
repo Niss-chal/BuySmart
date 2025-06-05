331301295221a158afe5037eddbf9856b52ee22b
Aaryaman01 <aaryamanshakya11@gmail.com>
2025-06-05 21:12:27 +0545

    added logout and back function in cart manage and dashboard

--- BuySmart/src/buysmart/controller/CartManageController.java
+++ BuySmart/src/buysmart/controller/CartManageController.java
@@ -5,6 +5,10 @@
 package buysmart.controller;
 
 import buysmart.view.CartManage;
+import buysmart.view.Dashboard;
+import buysmart.view.LoginView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 
@@ -17,6 +21,10 @@ public class CartManageController {
 
     public CartManageController(CartManage cartmanage){
         this.cartmanage=cartmanage;
+        Logout logout = new Logout();
+        this.cartmanage.logout(logout);
+        Back back = new Back();
+        this.cartmanage.back(back);
 }
 
     public void open(){
@@ -26,5 +34,26 @@ public class CartManageController {
     public void close(){
         cartmanage.dispose();
     }
-    
+    class Logout implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cartmanage.dispose();
+            LoginView loginview = new LoginView();
+            LoginController loginController = new LoginController(loginview);
+            loginController.open();
+            
+        }
+}
+    class Back implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cartmanage.dispose();
+            Dashboard dashboard = new Dashboard();
+            DashboardController dashboardController = new DashboardController(dashboard);
+            dashboardController.open();
+            
+        }
+    }
 }
