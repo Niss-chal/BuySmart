68269c69e8f8a1b39f90f0c09db5c80275cd76be
NameLess <niachaldahal2062@gmail.com>
2025-06-06 11:57:57 +0545

    Fixed Error

--- BuySmart/src/buysmart/controller/LoginController.java
+++ BuySmart/src/buysmart/controller/LoginController.java
@@ -86,7 +86,6 @@ if(email.isEmpty() || password.isEmpty()){
 
             }
             
-        }
     
     class ForgotPassword implements MouseListener{
 
@@ -100,23 +99,27 @@ if(email.isEmpty() || password.isEmpty()){
         }
 
         @Override
-        public void mousePressed(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-        @Override
         public void mouseEntered(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            // TODO Auto-generated method stub
+        
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
         }
         
     }
@@ -132,23 +135,32 @@ if(email.isEmpty() || password.isEmpty()){
         }
 
         @Override
-        public void mousePressed(MouseEvent e) {
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-        }
-
-        @Override
         public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+         
         }
         
     }
     
+}
 
     
   
