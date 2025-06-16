00eec22f2a9c7822d69d31b4ebcabbf50d351da5
sarina0018 <sarinabatas@gmail.com>
2025-06-16 17:27:42 +0545

    Few changes and solve error

--- BuySmart/src/buysmart/database/MysqlConnection1.java
+++ BuySmart/src/buysmart/database/MysqlConnection1.java
@@ -14,9 +14,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MysqlConnection1 {
-    private static final String URL = "jdbc:mysql://localhost:3306/buysmart?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/buysmart?"
+            + "useSSL=false&allowPublicKeyRetrueval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "Captainrifal33@";
+    private static final String PASSWORD = "admin";
 
     public static Connection getConnection() throws SQLException {
         try {
