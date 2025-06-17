c45a1684f6b1c0248ff8d38e9380ba923e39a874
Niss-chal <240378@softwarica.edu.np>
2025-06-17 21:34:29 +0545

    made controller to view user profile

--- BuySmart/src/buysmart/controller/UserprofileController.java
+++ BuySmart/src/buysmart/controller/UserprofileController.java
@@ -4,6 +4,9 @@
  */
 package buysmart.controller;
 
+import buysmart.dao.UserprofileDAO;
+import buysmart.model.UserModel;
+import buysmart.view.UserprofileView;
 import buysmart.view.userprofileview;
 
 /**
@@ -11,10 +14,14 @@ import buysmart.view.userprofileview;
  * @author loq
  */
 public class UserprofileController { 
+    private UserprofileView view;
+    private String username;
     private userprofileview view;
 
     public UserprofileController(userprofileview view){
         this.view = view;
+        this.username=username;
+        init();
     }
 
     public void open(){
@@ -23,5 +30,22 @@ public class UserprofileController {
 
     public void close(){
         view.dispose();
+    }
+    
+    public void init (){
+       UserprofileDAO dao = new UserprofileDAO();
+       UserModel user = dao.getUserByUsername(username);
+       
+        if(user!=null){
+        view.getUpdatename().setText(user.getUsername());
+        view.getUpdateemail().setText(user.getEmail());
+        view.getUpdateaddress().setText(user.getAddress());
+        view.getUpdatecontact().setText(user.getContact());    
+        }
+        else{
+            javax.swing.JOptionPane.showMessageDialog(view,"User Profile not found! ","Error",javax.swing.JOptionPane.ERROR_MESSAGE);
+            view.dispose();
+        }
+    
     }
 }
