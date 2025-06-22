a22c1f081122304d13b89dc85abaeafb2880f6f1
Avay Lama <avaytamang27@gmail.com>
2025-06-22 22:53:39 +0545

    computers model implemented with getters method

---
+++ BuySmart/src/buysmart/model/computersModel.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buysmart.model;
+
+/**
+ *
+ * @author fahmi
+ */
+public class computersModel {
+    private String imagePath;
+    private String description;
+    private double price;
+
+    public computersModel(String imagePath, String description, double price) {
+        this.imagePath = imagePath;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+}
