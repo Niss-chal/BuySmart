1aadffbb03614125fd3f843ede812f2883fed6c0
PranayaCodes <khadkapranaya14@gmail.com>
2025-06-22 09:18:01 +0545

    Added back button functionality

--- BuySmart/src/buysmart/controller/ChangePasswordController.java
+++ BuySmart/src/buysmart/controller/ChangePasswordController.java
@@ -4,10 +4,42 @@
  */
 package buysmart.controller;
 
+import buysmart.view.ChangePassword;
+import buysmart.view.Dashboard;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author User
  */
 public class ChangePasswordController {
+    
+    private ChangePassword view;
+    private String email;
+    
+    public ChangePasswordController(ChangePassword view,String email){
+        this.view=view;
+        this.email=email;
+        PasswordBack changeBack = new PasswordBack();
+        this.view.changeBack(changeBack);
+        
+        
+        
+    }
+    
+    
+    class PasswordBack implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.dispose();
+            Dashboard dashboard = new Dashboard();
+            DashboardController dashboardcontroller = new DashboardController(dashboard,email);
+            dashboardcontroller.open();
+        }
+        
+    }
+    
     
 }
