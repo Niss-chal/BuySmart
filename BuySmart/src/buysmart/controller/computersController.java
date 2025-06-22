2ed2cb4c3e31ef28e46f2b5f658f6a1d44a1008a
Avay Lama <avaytamang27@gmail.com>
2025-06-23 01:20:15 +0545

    Revert "removed image part"
    This reverts commit 25a686f025f3c63c4f4fb958a11e79fb9d529951.

--- BuySmart/src/buysmart/controller/computersController.java
+++ BuySmart/src/buysmart/controller/computersController.java
@@ -8,7 +8,6 @@ import buysmart.dao.computersDAO;
 import buysmart.model.computersModel;
 import buysmart.view.ComputersView;
 import java.util.List;
-import javax.swing.JOptionPane;
 
 
 /**
@@ -31,17 +30,11 @@ public class computersController {
         try {
             computers = computersDAO.getComputers();
             if (!computers.isEmpty()) {
-                if (computers.isEmpty()) {
-                    JOptionPane.showMessageDialog(computersView, "No computers found in the database.", "Warning", JOptionPane.WARNING_MESSAGE);
-                    computersView.getComputersPrice1().setText("No computers found");
-                    return;
-                }
-                
                 // 1st computer
                 if (computers.size() > 0) {
                     computersModel computer0 = computers.get(0);
                     if (computer0 != null) {
-//                        computersView.getComputersPicture1().setText(computer0.getImagePath());
+                        computersView.getComputersPicture1().setText(computer0.getImagePath());
                         computersView.getComputersDescription1().setText(computer0.getDescription());
                         computersView.getComputersPrice1().setText("Rs. " + computer0.getPrice());
                     }
@@ -50,7 +43,7 @@ public class computersController {
                 if (computers.size() > 1) {
                     computersModel computer1 = computers.get(1);
                     if (computer1 != null) {
-//                        computersView.getComputersPicture2().setText(computer1.getImagePath());
+                        computersView.getComputersPicture2().setText(computer1.getImagePath());
                         computersView.getComputersDescription2().setText(computer1.getDescription());
                         computersView.getComputersPrice2().setText("Rs. " + computer1.getPrice());
                     }
@@ -59,7 +52,7 @@ public class computersController {
                 if (computers.size() > 2) {
                     computersModel computer2 = computers.get(2);
                     if (computer2 != null) {
-//                        computersView.getComputersPicture3().setText(computer2.getImagePath());
+                        computersView.getComputersPicture3().setText(computer2.getImagePath());
                         computersView.getComputersDescription3().setText(computer2.getDescription());
                         computersView.getComputersPrice3().setText("Rs. " + computer2.getPrice());
                     }
@@ -68,7 +61,7 @@ public class computersController {
                 if (computers.size() > 3) {
                     computersModel computer3 = computers.get(3);
                     if (computer3 != null) {
-//                        computersView.getComputersPicture4().setText(computer3.getImagePath());
+                        computersView.getComputersPicture4().setText(computer3.getImagePath());
                         computersView.getComputersDescription4().setText(computer3.getDescription());
                         computersView.getComputersPrice4().setText("Rs. " + computer3.getPrice());
                     }
@@ -77,7 +70,7 @@ public class computersController {
                 if (computers.size() > 4) {
                     computersModel computer4 = computers.get(4);
                     if (computer4 != null) {
-//                        computersView.getComputersPicture5().setText(computer4.getImagePath());
+                        computersView.getComputersPicture5().setText(computer4.getImagePath());
                         computersView.getComputersDescription5().setText(computer4.getDescription());
                         computersView.getComputersPrice5().setText("Rs. " + computer4.getPrice());
                     }
